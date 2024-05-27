818a732bf1f5e74b42afbe04dc6b186267c5b1ef
123solangelo <hodayah2610@gmail.com>
2024-05-27 19:58:50 +0300

    po

--- loginpage.js
+++ loginpage.js
@@ -5,12 +5,12 @@ function myFunction(){
     var email1Input = document.getElementById("email1");
     var userName1Input = document.getElementById("username1");
   if(email1Input.value == "shaniwisch@gmail.com" && password1Input.value == "1234"){
-    window.alert("Hello",userName1Input.value," Welcome to our book site!");
-    window.location.href = "html/home_page.html";
+    window.alert("Hello"+ userName1Input.value + " Welcome to our book site!");
+    window.location.href = "homepage.html";
   }
   if(email1Input.value == "test" && password1Input.value == "test"){
-    window.alert("Hello",userName1Input.value," Welcome to our book site!");
-    window.location.href = "html/home_page.html";
+    window.alert("Hello " + userName1Input.value+ " Welcome to our book site!");
+    window.location.href = "homepage.html";
   }
     
   else{window.alert("Incorrect email or password, try other data");}
