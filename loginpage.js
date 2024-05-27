590b73771f70faaf5f360fae0e1144f00ef4e556
shaniwisch <shaniwisch@gmail.com>
2024-05-27 18:51:43 +0300

    hi im starting

---
+++ loginpage.js
@@ -0,0 +1,17 @@
+
+
+function myFunction(){
+    var password1Input = document.getElementById("password1");
+    var email1Input = document.getElementById("email1");
+    var userName1Input = document.getElementById("username1");
+  if(email1Input.value == "shaniwisch@gmail.com" && password1Input.value == "1234"){
+    window.alert("Hello",userName1Input.value," Welcome to our book site!");
+    window.location.href = "html/home_page.html";
+  }
+  if(email1Input.value == "test" && password1Input.value == "test"){
+    window.alert("Hello",userName1Input.value," Welcome to our book site!");
+    window.location.href = "html/home_page.html";
+  }
+    
+  else{window.alert("Incorrect email or password, try other data");}
+  }
