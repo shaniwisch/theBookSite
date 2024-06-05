14eacb36a4d7c58fad8daed6106bcf813988ad72
shaniwisch <shaniwisch@gmail.com>
2024-06-05 19:19:28 +0300

    hi

---
+++ horror.js
@@ -0,0 +1,9 @@
+function homepage(){
+    window.location.href = "homepage.html";
+  
+  }
+  
+  function logOut(){
+    window.location.href = "index.html";
+  
+  }
--- loginpage.js
+++ loginpage.js
@@ -54,12 +54,3 @@ function myFunction(){
 
 
 
-function homepage(){
-  window.location.href = "homepage.html";
-
-}
-
-function logOut(){
-  window.location.href = "index.html";
-
-}
